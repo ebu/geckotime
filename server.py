7e06f33ef6ae31d77e2497353915dd1011e3b9dd
gcmalloc <gcmalloc@gmail.com>
2013-08-06 17:00:41 +0200

    adding server

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
+from dateutil import rrule
+import datetime
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n\
-            <HTML>\n\
-            <HEAD><TITLE>Python on cloudControl</TITLE></HEAD>\n\
-            <BODY>\n\
-            <center><h1>Hello World!</h1></center>\n\
-            </BODY>\n\
-            </HTML>"
+def home():
+    return "/hour/minute return a dict"
+
+@app.route('/<int:hour>/<int:minute>')
+def hour(hour, minute):
+    t = datetime.datetime(2013, 1, 1, hour, minute, 0, 0)
+    r = rrule.rrule(rrule.DAILY, t)
+    dt = r.after(datetime.datetime.now()) - datetime.datetime.now()
+    return jsonify({'item':[{'value':dt.seconds//3600, 'description':'hours'},{'value':(dt.seconds//60)%60, 'description':'minutes'}]})
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
