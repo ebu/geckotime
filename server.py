94e92c80e79c8b594288854a611d83a2ecebe507
gcmalloc <gcmalloc@gmail.com>
2013-08-16 19:13:18 +0200

    fix

--- server.py
+++ server.py
@@ -11,17 +11,23 @@ API_JSON = ""
 
 class GithubStatus():
     BASE_URL = "https://status.github.com/api.json"
-    STATUS_TRANSLATE = defaultdict(good='Up')
+
+
     def __init__(self):
         logging.info("fetching the base url for github status")
-        _, _, self.last_message_url = requests.get(GithubStatus.BASE_URL).json()
+        _, _, self.last_message_url = requests.get(GithubStatus.BASE_URL).json().values()
 
 
     @property
     def json(self):
         last_message = requests.get(self.last_message_url).json()
-        last_message['status'] = GithubStatus.STATUS_TRANSLATE[last_message['status']]
-        return {'status':'up', 'responseTime':last_message['message']}
+        if last_message['status'] == 'good':
+            status = 'Up'
+        else:
+            status = 'Down'
+
+        message = last_message['body']
+        return {'status':status, 'responseTime':message}
 
 status = GithubStatus()
 
@@ -38,7 +44,7 @@ def hour(hour, minute):
 
 @app.route('/github')
 def github():
-    return status.json
+    return jsonify(status.json)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
