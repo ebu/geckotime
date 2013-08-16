36049bddb45c62452a98535dd6c77fcc01055f34
gcmalloc <gcmalloc@gmail.com>
2013-08-16 18:24:04 +0200

    adding github status support

--- server.py
+++ server.py
@@ -2,8 +2,28 @@ import os
 from flask import Flask, jsonify
 from dateutil import rrule
 import datetime
+import requests
+from collections import defaultdict
+import logging
 
 app = Flask(__name__)
+API_JSON = ""
+
+class GithubStatus():
+    BASE_URL = "https://status.github.com/api.json"
+    STATUS_TRANSLATE = defaultdict(good='Up')
+    def __init__(self):
+        logging.msg("fetching the base url for github status")
+        _, _, self.last_message_url = requests.get(GithubStatus.BASE_URL).json()
+
+
+    @property
+    def json(self):
+        last_message = requests.get(self.last_message_url).json()
+        last_message['status'] = GithubStatus.STATUS_TRANSLATE[last_message['status']]
+        return {'status':'up', 'responseTime':last_message['message']}
+
+status = GithubStatus()
 
 @app.route('/')
 def home():
@@ -16,6 +36,9 @@ def hour(hour, minute):
     dt = r.after(datetime.datetime.now()) - datetime.datetime.now()
     return jsonify({'item':[{'value':dt.seconds//3600, 'description':'hours'},{'value':(dt.seconds//60)%60, 'description':'minutes'}]})
 
+@app.route('/github')
+def github():
+    return status.json
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
