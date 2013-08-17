3c6b7a820e97d769d6d4e878d01fecce379a033f
gcmalloc <gcmalloc@gmail.com>
2013-08-17 15:52:30 +0200

    ahahhhhhhh debug mode

--- server.py
+++ server.py
@@ -1,84 +1,59 @@
 import os
 import datetime
-import logging
+from github_status import GithubStatus
 
 from dateutil import rrule
 import requests
 import lxml.html
 from flask import Flask, jsonify, redirect
+from flask.ext.cache import Cache
 
 app = Flask(__name__)
-API_JSON = ""
-
-class GithubStatus():
-    BASE_URL = "https://status.github.com/api.json"
-
-
-    def __init__(self):
-        logging.info("fetching the base url for github status")
-        urls = requests.get(GithubStatus.BASE_URL).json()
-        self.status_url = urls['status_url']
-        self.messages_url = urls['messages_url']
-
-
-    @property
-    def status(self):
-        last_message = requests.get(self.status_url).json()
-        if last_message['status'] == 'good':
-            status = 'Up'
-        else:
-            status = 'Down'
-
-        return {'status':status}
-
-    @property
-    def message(self):
-        messages = requests.get(self.messages_url).json()
-        out = []
-        for m in messages:
-            summary = {}
-            if m['status'] == 'good':
-               summary['type'] = 0
-            elif m['status'] == 'minor':
-               summary['type'] = 1
-            else:
-               summary['type'] = 2
-            t = m['created_on']
-            summary['text'] = m['body'] + "\n" + t.replace('T', ' ').replace('Z', '')
-            out.append(summary)
-        return {'item':out}
-
 status = GithubStatus()
+cache = Cache(app)
 
 @app.route('/')
 def home():
-    return "/hour/minute return a dict for geckoboard\n/github for more on github"
+    return ("/hour/minute return a dict for geckoboard" 
+           "/github for more on github")
 
 @app.route('/<int:hour>/<int:minute>')
 def hour(hour, minute):
     t = datetime.datetime(2013, 1, 1, hour, minute, 0, 0)
     r = rrule.rrule(rrule.DAILY, t)
     dt = r.after(datetime.datetime.now()) - datetime.datetime.now()
-    return jsonify({'item':[{'value':dt.seconds//3600, 'text':'hours'},{'value':(dt.seconds//60)%60, 'text':'minutes'}]})
+    return jsonify( { 'item':[{ 'value':dt.seconds//3600, 'text':'hours'},
+                    { 'value':(dt.seconds//60)%60, 'text':'minutes'}]})
 
 @app.route('/github/')
 def github():
+    """Documentation for the github view
+    """
     return "/github/status for status (up or down)\n/github/messages for detailed message"
 
 @app.route('/github/status')
+@cache.cached(timeout=120)
 def github_status():
+    """Return a geckoboard compatible message containing the status of github services
+    """
     return jsonify(status.status)
 
 @app.route('/github/messages')
+@cache.cached(timeout=120)
 def github_msg():
+    """Return a geckoboard compatible message list send on github status
+    """
     return jsonify(status.message)
 
 @app.route('/xkcd')
+@cache.cached(timeout=900)
 def xkcd():
+    """Return the latest xkcd comic. It will redirect the request to the image url.
+    """
     xkcd_page = requests.get('http://xkcd.com').content
     html = lxml.html.fromstring(xkcd_page)
     return redirect(html.xpath('//div[@id="comic"]/img/@src')[0])
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
